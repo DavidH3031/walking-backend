9c4ad4d743ed6dbff2c886a62f0f970732db0ba4
Keuykanard <zigzag1@live.co.uk>
2023-01-03 16:30:30 +0000

    updated backend

--- models/User.js
+++ models/User.js
@@ -8,6 +8,7 @@ const UserSchema = new Schema({
   coins: { type: Number, required: true, default: 100 },
   trophies: { type: Array, required: true, default: [] },
   quests: { type: Array, required: true, default: [] },
+  equippedArmour: { type: String, default: "basic" },
 });
 
 exports.User = mongoose.model("User", UserSchema);
